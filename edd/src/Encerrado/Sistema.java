d48aea05bd1e6303c4d6c4dbeb69d79b52c8689b
Mauricio Rubio Haro <mauricio.rubio@ciencias.unam.mx>
2022-05-14 20:59:04 -0500

    Validar si o no

--- edd/src/Encerrado/Sistema.java
+++ edd/src/Encerrado/Sistema.java
@@ -1,17 +1,55 @@
 package edd.src.Encerrado;
 
-public class Sistema {
-    Juego juego = new Juego();
-    Jugador turno;
+import java.lang.Character;
+import java.util.Scanner;
 
-    public Sistema(){
-        iniciarJuego();
+public class Sistema {
+
+  Scanner sc;
+  Juego juego = new Juego();
+  Jugador turno;
+
+  public Sistema() {
+    iniciarJuego();
+  }
+
+  public void iniciarJuego() {
+    System.out.println("BIENVENIDO A ENCERRADOS");
+    System.out.println("\n Este es el tablero inicial");
+    System.out.println(juego.getTablero());
+    System.out.println(
+      "\n Este es el tablero inicial ¿Deseas mantenerlo asi? (S/N)"
+    );
+    if (validarSioNo()) {
+        comenzarJuego();
+    }else{
+        asignarTablero();
     }
-    public void iniciarJuego(){
-        System.out.println("BIENVENIDO A ENCERRADOS");
-        System.out.println("\n Este es el tablero inicial");
-        //System.out.println(juego);
-        System.out.println(juego.getTablero());
-        System.out.println("\n Este es el tablero inicial ¿Deseas mantenerlo asi?");
-    }
+  }
+
+  public void comenzarJuego(){
+
+  }
+
+
+  public boolean validarSioNo() {
+    char aux;
+    boolean condicion = false;
+    sc = new Scanner(System.in);
+    do {
+      try {
+        aux = sc.nextLine().charAt(0);
+        aux = Character.toLowerCase(aux);
+        if (aux == 's') {
+          return true;
+        } else if (aux == 'n') {
+          return false;
+        }
+      } catch (Exception e) {
+        System.out.println("Ingresa una opcion valida");
+        condicion = false;
+      }
+    } while (!condicion);
+    return false;
+  }
 }
